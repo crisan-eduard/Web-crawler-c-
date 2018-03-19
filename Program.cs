da7999a506327faa3e3c21be8d59c6841d375d3b
crisan-eduard <37360432+crisan-eduard@users.noreply.github.com>
2018-03-19 12:22:51 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using HtmlAgilityPack;
 
 namespace crawler
@@ -79,22 +80,22 @@ namespace crawler
         {
             delete_file_content(file_path);
             foreach(String link in links)
-                System.IO.File.AppendAllText(file_path,link);
-
+                System.IO.File.AppendAllText(file_path,link+"\n");
+                                                   //added \n ^
         }
     }
 
     class LinkFinder
     {
-        //get url and return all links from it (formated, and really nice)
+        //get url and return all links from it (good format, really nice)
         private String url;
-        private List<String> links = new List<string>();
+        private HashSet<String> links = new HashSet<string>();
         public LinkFinder(String url)
         {
             this.url = url;
         }
 
-        public List<String> get_links()
+        public HashSet<String> get_links()
         {
             Uri uri;
             uri = new Uri(this.url);
@@ -149,21 +150,24 @@ namespace crawler
 
     class Spider
     {
-        private static String project_name="", base_url="", domain_name="", queue_file="", crawled_file="";
+        private static String project_name = "", base_url = "", queue_file = "", crawled_file = "";//, domain_name = "";
         private static HashSet<String> queue = new HashSet<string>();
-        private static List<String> crawled = new List<string>();
+        private static HashSet<String> crawled = new HashSet<string>();
         //class variables
 
-        public Spider(String project_name_in, String base_url_in, String domain_name_in)
+        public Spider(String project_name_in, String base_url_in)//, String domain_name_in)
         {
             project_name = project_name_in;
             base_url = base_url_in;
-            domain_name = domain_name_in;
+           // domain_name = domain_name_in;
             queue_file = project_name + "/queue.txt";
             crawled_file = project_name + "/crawled.txt";
+
+            boot();
+            crawl_page("first spider", base_url);
         }
 
-        public void boot()
+        public static void boot()
         {
             //if you are the very first spider, you have to first create the proj directory, create the 2 data files
             Functions FilesFolders = new Functions();
@@ -172,29 +176,155 @@ namespace crawler
 
             //don't read from file everytime, read from file and convert to set/list
             queue = FilesFolders.file_to_set(queue_file);
-            crawled = FilesFolders.file_to_list(crawled_file);
+            crawled = FilesFolders.file_to_set(crawled_file);
         }
 
-        public void crawl_page()
+        public static void crawl_page(String thread_name, String page_url)
+        {
+            if(!crawled.Contains(page_url))
+            {
+                Console.WriteLine(thread_name + " crawling " + page_url);
+                Console.WriteLine("Queue "+queue.Count.ToString()+" | Crawled "+crawled.Count.ToString());
+                Spider.add_links_to_queue(Spider.gather_links(page_url));
+                Spider.queue.Remove(page_url);
+                Spider.crawled.Add(page_url);
+                //now we need to update the files
+                Spider.update_files();
+            }
+        }
+
+        public static HashSet<String> gather_links(String url)
+        {
+            try
+            {
+                LinkFinder linkfinder = new LinkFinder(url);
+                HashSet<String> links = linkfinder.get_links();
+                return links;
+                //return set of links
+            }catch
+            {
+                Console.WriteLine("Can not crawl page "+url);
+                HashSet<String> emptyset = new HashSet<string>();
+                return emptyset;
+                //return emptyset
+            }
+        }
+
+        public static void add_links_to_queue(HashSet<String> links)
+        {
+            foreach(String url in links)
+            {
+                //make sure they ar enot in waiting list, crawled list
+                if (Spider.queue.Contains(url))
+                    continue;
+                if (Spider.crawled.Contains(url))
+                    continue;
+
+                /*
+                 * if you want to crawl a specific website, test here for domain
+                 */
+                Spider.queue.Add(url);
+            }
+        }
+
+        public static void update_files()
+        {
+            Functions FilesFolders = new Functions();
+            FilesFolders.set_to_file(Spider.queue, Spider.queue_file);
+            FilesFolders.set_to_file(Spider.crawled, Spider.crawled_file);
+        }
+
+    }
+
+    class Webcrawler
+    {
+
+        public static String path = @"D:\FOLDERS\PROGRAMMING\CSHARP\web crawler 3\databases";
+
+        public static String PROJECT_NAME = "webcrawler";
+        public static String HOME_PAGE = "http://www.cprarad.ro/";
+        public static String QUEUE_FILE = path + "/queue.txt";
+        public static String CRAWLED_FILE = path + "/crawled.txt";
+        public static int NUMBER_OF_THREADS = 2;
+
+        Spider spider1 = new Spider(path, HOME_PAGE);
+        static Queue<String> queue = new Queue<String>();
+        //thread queue
+        public void crawl()
         {
 
+            //Spider spider1 = new Spider(path, HOME_PAGE);
+            //first spider
+
+            //check if there are items in the queue file, the crawl
+            Functions FilesFolders = new Functions();
+            HashSet<String> queued_links = new HashSet<string>();
+            queued_links = FilesFolders.file_to_set(QUEUE_FILE);
+            if (queued_links.Count > 0)
+                Console.WriteLine("crwaling...");
+                create_jobs();
         }
 
-        boot();
-        crawl_page("first spider", base_url);
+        public void create_jobs()
+        {
+            Functions FilesFolders = new Functions();
+            foreach (String link in FilesFolders.file_to_set(QUEUE_FILE))
+            {
+                queue.Enqueue(link);
+            }
+            //queue.Join();
+            crawl();
+        }
+
+        public void create_workers()
+        {
+            for (int i = 0; i < NUMBER_OF_THREADS; ++i)
+            {
+                Thread t = new Thread(work);
+                t.IsBackground = true;
+                t.Start();
+            }
+        }
+
+        public void work()
+        {
+            //do the next job in the queue
+
+            while (true)
+            {
+                String url = queue.Dequeue();
+                Spider.crawl_page(Thread.CurrentThread.Name, url);
+                //task_done
+            }
+        }
     }
 
     class Program
     {
-        
 
         static void Main(string[] args)
         {
-            String path = @"D:\FOLDERS\PROGRAMMING\CSHARP\web crawler 3\databases";
+            // Queue<>
+            //threa queue
 
-            Functions f = new Functions();
-            f.create_dir(path);
-            f.create_data_files(path, "www.cprarad.ro");
+            //the first thing ut needs to do, is to crawl homepage
+            // Spider spider1 = new Spider(path, HOME_PAGE);
+
+            /*HashSet<String> test = new HashSet<string>();
+            LinkFinder finder = new LinkFinder("http://www.cprarad.ro/");
+            test=finder.get_links();
+            foreach(String link in test)
+            {
+                Console.WriteLine(link);
+            }
+            */
+
+            Webcrawler webcrawler1 = new Webcrawler();
+
+            webcrawler1.create_workers();
+            webcrawler1.crawl();
+
+
         }
     }
 }
